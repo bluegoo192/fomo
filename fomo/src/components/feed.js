7d66896d1a3d96dfa6a903c589a0d16051dfb78b
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-04-08 11:09:08 -0700

    basic feed logic, with stubs

--- fomo/src/components/feed.js
+++ fomo/src/components/feed.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+function FeedCard(props) {
+  return(
+    <View style={styles.card}>
+      <Text>{props.data.title}</Text>
+    </View>
+  );
+}
 
 export default class Feed extends React.Component {
-    static navigationOptions = {
-      title: 'Feed'
-    };
+  getFeed() {
+    var stubItems = [
+      {
+        "title": "test post 1"
+      },
+      {
+        "title": "test post 2"
+      },
+      {
+        "title": "test post 3"
+      }
+    ];
+    return(stubItems);
+  }
     render() {
+       var items = [];
+       this.getFeed().forEach(function(i, index) {
+         items.push(<FeedCard data={i} key={index} />);
+       });
         return(
-            <Text>FEEEEED</Text>
+          <View style={styles.container}>
+            {items}
+          </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  card: {
+    backgroundColor: '#ccc'
+  }
+
+});
