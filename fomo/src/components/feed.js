d1d81a406c1f110883c4c5808dd5da4448eacada
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-04-30 17:04:03 -0700

    removed unimported Animated reference

--- fomo/src/components/feed.js
+++ fomo/src/components/feed.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image, FlatList } from 'react-native';
 
-const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+//const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 function FeedCard(props) {
   return(
