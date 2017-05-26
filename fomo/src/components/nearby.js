06ac3f3d6c1121c49302a6ef637ba739ba341b58
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-05-26 10:33:03 -0700

    fixed react native maps. i think

--- fomo/src/components/nearby.js
+++ fomo/src/components/nearby.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class Nearby extends React.Component {
     static navigationOptions = {
