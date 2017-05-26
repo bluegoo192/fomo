0829191ddfb4cbef9d93dec5e40e102a4d70e3d5
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-05-26 10:40:10 -0700

    made map actually show up

--- fomo/src/components/nearby.js
+++ fomo/src/components/nearby.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default class Nearby extends React.Component {
     static navigationOptions = {
@@ -8,7 +21,16 @@ export default class Nearby extends React.Component {
     };
     render() {
         return(
-            <Text>Nearby</Text>
+          <View style ={styles.container}>
+            <MapView style={styles.map}
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }}
+            />
+          </View>
         );
     }
 }
